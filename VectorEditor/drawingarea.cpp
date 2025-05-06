a071aa605f7ce52b12c5bc5a9dfc3c38bec3ee1e
TikhonVasiliev <vasiliev7951149@gmail.com>
2025-05-06 16:29:02 +0300

    create drawing area

---
+++ VectorEditor/drawingarea.cpp
@@ -0,0 +1,44 @@
+#include "drawingarea.h"
+#include <QPainter>
+#include <QPen>
+#include <QVector2D>
+#include <QtMath>
+void DrawingArea::paintEvent(QPaintEvent* event)
+{
+    QPainter painter(this);
+    painter.fillRect(rect(), Qt::white);
+
+    QPen pen(Qt::blue, 2);
+    painter.setPen(pen);
+
+    const int centerX = width() / 2;
+    const int centerY = height() / 2;
+
+    painter.translate(centerX, centerY);
+
+    // Масштаб для наглядности векторов
+    const double scale = 20.0;
+
+    for (const auto& sptrVec : vectors) {
+        if (sptrVec) {
+            const Vector& vec = *sptrVec;
+            QPointF endPoint(vec.getX() * scale, -vec.getY() * scale);
+
+            // Рисуем стрелку от центра к точке
+            painter.drawLine(QPointF(0, 0), endPoint);
+
+            // Рисуем голову стрелки
+            const double arrowSize = 8.0;
+            QVector2D dir(endPoint);
+            QVector2D norm = dir.normalized();
+            QPointF arrowP1 =  endPoint - QPointF(norm.y(), -norm.x()) * arrowSize;
+            QPointF arrowP2 =  endPoint - QPointF(-norm.y(), norm.x()) * arrowSize;
+
+            QPolygonF arrowHead;
+            arrowHead << endPoint << arrowP1 << arrowP2;
+            painter.setBrush(Qt::blue);
+            painter.drawPolygon(arrowHead);
+            painter.setBrush(Qt::NoBrush);
+        }
+    }
+}
---
+++ VectorEditor/drawingarea.h
@@ -0,0 +1,30 @@
+#ifndef DRAWINGAREA_H
+#define DRAWINGAREA_H
+
+#include <QWidget>
+#include "vector.h"
+#include "smart_ptr.h"
+
+class DrawingArea : public QWidget {
+    Q_OBJECT
+
+private:
+    std::vector<smart_ptr<Vector>> vectors;
+
+public:
+    explicit DrawingArea(QWidget* parent = nullptr)
+        : QWidget(parent)
+    {
+        setMinimumSize(400, 400);
+    }
+
+    void setVectors(const std::vector<smart_ptr<Vector>>& vecs) {
+        vectors = vecs;
+        update();
+    }
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+};
+
+#endif // DRAWINGAREA_H
