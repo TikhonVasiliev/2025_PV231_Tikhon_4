34321d69135ab3e06b8ece06b57d6d2e1b31569a
TikhonVasiliev <vasiliev7951149@gmail.com>
2025-05-06 16:14:18 +0300

    create smart_ptr

--- VectorEditor/build/5_15_16_mingw64_static-Debug/qtcsettings.cmake
+++ VectorEditor/build/5_15_16_mingw64_static-Debug/qtcsettings.cmake
@@ -1,11 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_CXX_COMPILER" "C:/msys64/mingw64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/msys64/mingw64/qt5-static" CACHE "PATH" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/QtProject/Reps/2025_PV231_Tikhon_4/VectorEditor/build/5_15_16_mingw64_static-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "C:/msys64/mingw64/qt5-static/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/msys64/mingw64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
---
+++ VectorEditor/smart_ptr.h
@@ -0,0 +1,34 @@
+#ifndef SMART_PTR_H
+#define SMART_PTR_H
+
+template <typename T>
+class smart_ptr {
+    T* obj;
+public:
+    explicit smart_ptr(T* obj = nullptr)
+        : obj(obj) { }
+
+    ~smart_ptr() {
+        delete obj;
+    }
+
+    // Конструктор копирования
+    smart_ptr(const smart_ptr& other) = default;
+
+    // Оператор присваивания копированием
+    smart_ptr& operator=(const smart_ptr& other) = default;
+
+    // Перемещающий конструктор
+    smart_ptr(smart_ptr&& other) noexcept = default;
+
+    // Перемещающий оператор присваивания
+    smart_ptr& operator=(smart_ptr&& other) noexcept = default;
+
+    // Доступ к объекту
+    T* operator->() const { return obj; }
+    T& operator*() const { return *obj; }
+    T* get() const { return obj; }
+    explicit operator bool() const { return obj!=nullptr; }
+};
+
+#endif // SMART_PTR_H
